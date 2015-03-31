40b4519be767dcc6f23746e5629285d15836344c
Daniel Stern <daniel@danielstern.ca>
2015-03-31 17:36:03 -0400

    whaaaat

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,58 +10,112 @@ function illustrateArray(data,svg){
 	var container = svg.append('g')
 		.attr("transform",function(d,i){return "translate(0,50)"})
 
-	function drawNumbers(data){
-		var rep = container.selectAll('g.rep')
-			.data(data)
+	var elements;
+	var avatars;
+	var commas;
+	var parens;
+
+	// function draw(data) {
+	// 	elements = container.selectAll('g.element')
+	// 		.data(data)
+	// 		.enter().append('g')
+	// 		.attr('class','element')
+	// 		.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
+
+	// 	avatars = elements.append('text')
+	// 		.text(function(d,i){return d})
+	// 		.attr('class','avatar')
+	// 		.attr("fill",function(a){return color(0)})
+
+	// 	commas = elements.append('text')
+	// 		.text(function(d,i){return ','})
+	// 		.attr("fill",function(a){return color(1)})	
+	// 		.attr("x",function(a){return 20})	
+	// 		.attr("opacity",function(a,i){return i==data.length-1 ? 0 : 1})	
+
+	// 	parens = container.selectAll('g.parens')
+	// 		.data(['[',']'])
+	// 		.enter().append('g')
+	// 		.attr('class','parens')
+	// 		.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
+
+	// 	parens.append('text')
+	// 		.text(function(d,i){return d})
+	// 		.attr("fill",function(a){return color(2)})
+	// }
+
+	function update(data) {
+
+		// DATA JOIN
+		var elements = container.selectAll('g.element')
+			.data(data,function(d,i){return i});
+
+		elements.selectAll('text').data(data,function(d,i){return i});
+
+			
+		// UPDATE
+		elements
+			.selectAll('text')
+			.text(function(d,i){return d})
+
+
+		// debugger;
+		// ENTER
+		elements
 			.enter().append('g')
-			.attr('class','rep')
+			.attr('class','element')
 			.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
-
-		rep.append('text')
+			.append('text')
 			.text(function(d,i){return d})
+			.attr('class','avatar')
 			.attr("fill",function(a){return color(0)})
 
-		rep.append('text')
-			.text(function(d,i){if (i < data.length-1) return ","})
-			.attr("fill",function(a){return color(0)})
-			.attr("x",function(a){return 20})
+		
+
+		// elements.exit().remove()
+
+
+		// elements
+		// 	.data(data)
+		// 	.enter()
+		// 	.append('g')
+		// 	.attr('class','element')
+		// 	.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
+
+		// elements
+		// 	.selectAll('g.avatar')
+		// 	.text(function(d,i){return d})
+		// 	.attr('class','avatar')
+		// 	.attr("fill",function(a){return color(0)})
+
+		// commas
+		// 	.append('text')
+		// 	.text(function(d,i){return ','})
+		// 	.attr("fill",function(a){return color(1)})	
+		// 	.attr("x",function(a){return 20})	
+		// 	.attr("opacity",function(a,i){return i==data.length-1 ? 0 : 1})	
+
+		// elements.selectAll('text')
+		// 	.text(function(d,i){return d})
+		// 	.attr("fill",function(a){return color(0)})
+
+		// parens
+		// 	.transition()
+		// 	.duration(1000)
+		// 	.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
 	}
 
-	function drawParens(data){
-		var parens = container.selectAll('g.parens')
-			.data(['[',']'])
-			.enter().append('g')
-			.attr('class','parens')
-			.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
-
-		parens.append('text')
-			.text(function(d,i){return d})
-			.attr("fill",function(a){return color(1)})
-
-	}
-
-	function updateParens(data){
-		var parens = container.selectAll('g.parens')
-			.transition()
-			.duration(1000)
-			.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
-
-	}
 
 
-	drawNumbers(data);
-	drawParens(data);
+
+
+	
+
+	update(data)
 
 	return {
 		container:container,
-		update:function(data){
-				drawNumbers(data);
-				drawParens(data);
-
-				updateParens(data);
-
-
-		}
+		update:update
 	};
 }
 
@@ -74,4 +128,4 @@ var a = illustrateArray(data,svg);
 var b =  illustrateArray(data.filter(filter),svg);
 b.container.attr("transform",function(d,i){return "translate(65,110)"})
 
-a.update([1,2,3,4,5,6])
+setTimeout(function(){a.update(['a','b','c','d','e','f'])},1500);
