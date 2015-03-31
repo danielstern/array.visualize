b598aa90f214c17302a444914ffd48013b330697
Daniel Stern <daniel@danielstern.ca>
2015-03-31 17:58:59 -0400

    cuttin the jank down

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,51 +15,21 @@ function illustrateArray(data,svg){
 	var commas;
 	var parens;
 
-	// function draw(data) {
-	// 	elements = container.selectAll('g.element')
-	// 		.data(data)
-	// 		.enter().append('g')
-	// 		.attr('class','element')
-	// 		.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
-
-	// 	avatars = elements.append('text')
-	// 		.text(function(d,i){return d})
-	// 		.attr('class','avatar')
-	// 		.attr("fill",function(a){return color(0)})
-
-	// 	commas = elements.append('text')
-	// 		.text(function(d,i){return ','})
-	// 		.attr("fill",function(a){return color(1)})	
-	// 		.attr("x",function(a){return 20})	
-	// 		.attr("opacity",function(a,i){return i==data.length-1 ? 0 : 1})	
-
-	// 	parens = container.selectAll('g.parens')
-	// 		.data(['[',']'])
-	// 		.enter().append('g')
-	// 		.attr('class','parens')
-	// 		.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
-
-	// 	parens.append('text')
-	// 		.text(function(d,i){return d})
-	// 		.attr("fill",function(a){return color(2)})
-	// }
 
 	function update(data) {
 
 		// DATA JOIN
-		var elements = container.selectAll('g.element')
-			.data(data,function(d,i){return i});
+
+		var text = container.selectAll('text.avatar')
+			.data(data);
 
 		var parens = container.selectAll('g.parens')
 			.data(['[',']'])
 
-		// elements.selectAll('text').data(data,function(d,i){return i});
 
-			
 		// UPDATE
-		// elements
-		// 	.selectAll('text')
-		// 	.text(function(d,i){return d})
+		text
+			.text(function(d,i){return i})
 
 		parens
 			.transition()
@@ -67,61 +37,13 @@ function illustrateArray(data,svg){
 			.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
 
 
-		// debugger;
 		// ENTER
-		var news = elements
-			.enter().append('g')
-			.attr('class','element')
-			.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
-
-		news.append('text')
+		var newText = text.enter().append('text')
 			.text(function(d,i){return d})
 			.attr('class','avatar')
 			.attr("fill",function(a){return color(0)})
+			.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
 
-		news.append('text')
-			.text(function(d,i){return ','})
-			.attr("fill",function(a){return color(1)})	
-			.attr("x",function(a){return 20})	
-			.attr("opacity",function(a,i){return i==data.length-1 ? 0 : 1})	
-
-		parens
-			.enter().append('g')
-			.attr('class','parens')
-			.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
-
-		parens.append('text')
-			.text(function(d,i){return d})
-			.attr("fill",function(a){return color(2)})
-		
-
-		// EXIT
-		// elements.exit().remove()
-
-
-		// elements
-		// 	.data(data)
-		// 	.enter()
-		// 	.append('g')
-		// 	.attr('class','element')
-		// 	.attr("transform",function(d,i){return "translate("+(i*40+15)+",50)"})
-
-		// elements
-		// 	.selectAll('g.avatar')
-		// 	.text(function(d,i){return d})
-		// 	.attr('class','avatar')
-		// 	.attr("fill",function(a){return color(0)})
-
-		// commas
-		// 	.append('text')
-		// 	.text(function(d,i){return ','})
-		// 	.attr("fill",function(a){return color(1)})	
-		// 	.attr("x",function(a){return 20})	
-		// 	.attr("opacity",function(a,i){return i==data.length-1 ? 0 : 1})	
-
-		// elements.selectAll('text')
-		// 	.text(function(d,i){return d})
-		// 	.attr("fill",function(a){return color(0)})
 
 	
 	}
@@ -146,7 +68,7 @@ var svg = d3.select("#_filter")
 	.attr('height',600)
 	.attr('width',400);
 var a = illustrateArray(data,svg);
-var b =  illustrateArray(data.filter(filter),svg);
-b.container.attr("transform",function(d,i){return "translate(65,110)"})
+// var b =  illustrateArray(data.filter(filter),svg);
+// b.container.attr("transform",function(d,i){return "translate(65,110)"})
 
 setTimeout(function(){a.update(['a','b','c','d','e','f'])},1500);
