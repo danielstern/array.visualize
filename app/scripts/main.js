a37bbc0f0ad7e5fcb5fec7a20e3c1ef89a63b6cc
Daniel Stern <daniel@danielstern.ca>
2015-03-31 16:39:08 -0400

    D3 updating!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,17 +3,15 @@ var filter = function f(n){
 	return n%2===0;
 }
 
-function illustrateArray(data){
+function illustrateArray(data,svg){
 	
 	var color = d3.scale.category20b();
+	
+	var container = svg.append('g')
+		.attr("transform",function(d,i){return "translate(0,50)"})
 
-	var svg = d3.select("#_filter")
-		.append('svg')
-		.attr('height',600)
-		.attr('width',400);
-
-	function drawNumbers(){
-		var rep = svg.selectAll('g.rep')
+	function drawNumbers(data){
+		var rep = container.selectAll('g.rep')
 			.data(data)
 			.enter().append('g')
 			.attr('class','rep')
@@ -29,8 +27,8 @@ function illustrateArray(data){
 			.attr("x",function(a){return 20})
 	}
 
-	function drawParens(){
-		var parens = svg.selectAll('g.parens')
+	function drawParens(data){
+		var parens = container.selectAll('g.parens')
 			.data(['[',']'])
 			.enter().append('g')
 			.attr('class','parens')
@@ -42,26 +40,38 @@ function illustrateArray(data){
 
 	}
 
-	function drawCommas(){
-		var commas = svg.selectAll('g.commas')
-			.data(data.slice(0,data.length-1))
-			.enter().append('g')
-			.attr('class','comma')
-			.attr("transform",function(d,i){return "translate("+ (1+i) * 40 +",50)"})
-
-		commas.append('text')
-			.text(function(d,i){return ','})
-			.attr("fill",function(a){return color(2)})
+	function updateParens(data){
+		var parens = container.selectAll('g.parens')
+			.transition()
+			.duration(1000)
+			.attr("transform",function(d,i){return "translate("+ (i===0?(0*40):(data.length*40))+",50)"})
 
 	}
 
 
-	drawNumbers();
-	drawParens();
-	// drawCommas();	
+	drawNumbers(data);
+	drawParens(data);
 
+	return {
+		container:container,
+		update:function(data){
+				drawNumbers(data);
+				drawParens(data);
+
+				updateParens(data);
+
+
+		}
+	};
 }
 
 var data = [1,2,3,4,5];
-illustrateArray(data);
-illustrateArray(data.filter(filter));
+var svg = d3.select("#_filter")
+	.append('svg')
+	.attr('height',600)
+	.attr('width',400);
+var a = illustrateArray(data,svg);
+var b =  illustrateArray(data.filter(filter),svg);
+b.container.attr("transform",function(d,i){return "translate(65,110)"})
+
+a.update([1,2,3,4,5,6])
