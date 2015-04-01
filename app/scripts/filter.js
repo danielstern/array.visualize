f902638d07eb3dfbc97bf38b9a1dd55548992985
Daniel Stern <daniel@danielstern.ca>
2015-04-01 17:28:59 -0400

    filter viz so sick

--- app/scripts/filter.js
+++ app/scripts/filter.js
@@ -1,4 +1,5 @@
-(function filter(){
+(function filterDemo(){
+	console.log("filter!");
 	var data = [1,2,3,4,5,6,7,8];
 	var filter = function(n){return n%2};
 	var svg = d3.select("#_filter")
@@ -9,5 +10,30 @@
 	var a = illustrateArray(data,svg,{speed:250});
 	var filteredData = [];
 	var b = illustrateArray(filteredData,svg,{speed:250})
-	b.container.attr('transform',"translate(0,150)")
+	b.container.attr('transform',"translate(0,150)");
+
+	var index = 0;
+	var h = a.highlight(index);
+	h.color('blue')
+
+
+	var g = setInterval(function checkElement(){
+		var d = data[index];
+		h.goto(index);
+		if (filter(d)) {
+			h.color('green');
+			b.push(d);
+		} else {
+			h.color('red');
+		}
+		console.log("index?",index);
+		index++;
+
+		if (index > data.length) {
+			clearInterval(g);
+			svg.remove();
+			filterDemo();
+		}
+
+	},1500)
 })();
